92c43ca09aed82974dfcd60478dcf4d3cacc8688
Breno Ferreira <165968939+breno-hof@users.noreply.github.com>
2025-02-05 15:24:49 +0000

    update locals.tf

--- src/locals.tf
+++ src/locals.tf
@@ -1,9 +1,20 @@
 locals {
-	name				= "pool-monitoring"
-	openapi_file_path	= "${path.module}/openapi.yaml"
+	string_type = "S"
+	
+	name				= "pool_registry_table"
+
+	partition_key = {
+		name = "pool_id"
+		type = local.string_type
+	}
+
+	sort_key = {
+		name = "record_type"
+		type = local.string_type
+	}
 
 	tags				= {
 		Application		= local.name,
-		GitHubRepo		= "pool-monitoring-api-gateway"
+		GitHubRepo		= "pool-registry-service-dynamo-db"
 	}
 }
--- src/main.tf
+++ src/main.tf
@@ -1,8 +1,37 @@
 module "dynamo_db" {
-	source			= "../../module-dynamo-db/src"
+	source							= "../../module-dynamo-db/src"
 
-	name			= "pool-registry-table"
-	hash_key		= "pool-id"
-	hash_key_type	= "S"
+	name							= local.name
+	hash_key						= local.partition_key.name
+	hash_key_type					= local.partition_key.type
+	range_key						= local.sort_key.name
+	range_key_type					= local.sort_key.type
 	is_billing_mode_pay_per_request = true
+	is_ttl_enabled					= true
+	is_stream_enabled 				= true
+	is_deletion_protection_enabled	= false
+
+	global_secondary_index = [ {
+		index_name 					= "heating_type_index"
+		attribute_name 				= "heating_type"
+		attribute_type 				= local.string_type
+		range_key 					= local.partition_key.name
+		projection_type				= "ALL"
+	}, {
+		index_name 					= "material_index"
+		attribute_name 				= "material"
+		attribute_type 				= local.string_type
+		range_key 					= local.partition_key.name
+		projection_type				= "KEYS_ONLY"
+	} ]
+
+	local_secondary_index = [ {
+		index_name 					= "record_type_local_index"
+		range_key 					= local.sort_key.name
+		projection_type				= "ALL"
+	} ]
+
+	# replica = [ {
+	# 	region_name = "sa-east-1"
+	# } ]
 }
